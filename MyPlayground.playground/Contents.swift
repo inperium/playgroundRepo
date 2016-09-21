d571e12551f1ade63a22eccd400e6f48f5267647
inperium <ibow9416@740s-pl-111.local>
2016-09-21 11:40:17 -0600

    Created new Repo for playground.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var welcomeMessage: String
+var welcomeMessageTwo: String
+
+welcomeMessage = "😈"
+welcomeMessageTwo = "eh"
+
+while(true){
+
+print(welcomeMessage)
+sleep(1)
+
+print(welcomeMessageTwo)
+sleep(1)
+
+}
---
+++ MyPlaygroundTwo.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var what = "no"
+
+what = "what"
+
+var am = " am I doing?"
+
+var waitwhat = "Wait, \(what)\(am)"
+
+print(waitwhat)
+
+var characterCount = waitwhat.characters.count
+
+if(waitwhat == "Wait, what am I doing?")
+{
+    print(characterCount)
+}
