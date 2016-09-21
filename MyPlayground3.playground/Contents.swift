d49e9b4dba4c18dad3e1175abaaec593e171580a
inperium <ibow9416@740s-pl-111.local>
2016-09-21 12:32:10 -0600

    Added gray square in Playground.

--- MyPlayground3.playground/Contents.swift
+++ MyPlayground3.playground/Contents.swift
@@ -1,7 +1,6 @@
 import UIKit
 
-class Views: UIView {
-    
-    
-    
-}
+
+        let smallFrame = CGRect(x: 100, y: 100, width: 50, height: 50)
+        let graySquare = UIView(frame: smallFrame)
+        graySquare.backgroundColor = UIColor.darkGrayColor()
